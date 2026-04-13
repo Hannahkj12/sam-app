edb4ee8ba744756789a1f61345031f9aad989aea
Hannahkj12 <hannahkj2001@gmail.com>
2026-04-13 10:20:57 -0600

    Changed return message

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event, context) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello Hannah Johnson',
+        message: "I'm using canary deployments",
       })
     };
 
